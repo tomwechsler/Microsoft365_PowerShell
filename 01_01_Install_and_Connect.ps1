10ea627760cd88a0f4921b6e16af40c0a31c7430
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-03-25 14:02:52 +0100

    Add files via upload

---
+++ 01_01_Install_and_Connect.ps1
@@ -0,0 +1,27 @@
+﻿Set-Location c:\
+Clear-Host
+
+#Install the AzureADPreview
+Install-Module -Name AzureADPreview -AllowClobber -Verbose -Force
+
+#Connect without MFA
+$cred = Get-Credential 
+Connect-AzureAD -Credential $cred
+
+#Connect with MFA
+Connect-AzureAD -AccountId "tom@tomrocks.ch"
+
+#IMPORTANT => You cannot install both modules on the same system!
+
+#Install the AzureAD
+Install-Module -Name AzureAD -AllowClobber -Verbose -Force
+
+#Connect without MFA
+$cred = Get-Credential 
+Connect-AzureAD -Credential $cred
+
+#Connect with MFA
+Connect-AzureAD -AccountId "tom@tomrocks.ch"
+
+#Did it work?
+Get-AzureADUser
