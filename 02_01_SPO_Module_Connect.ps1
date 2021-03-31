4b06fced34cc4bf5a317d0405e99be6f983f156e
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-03-31 07:44:18 +0200

    Add files via upload

---
+++ 02_01_SPO_Module_Connect.ps1
@@ -0,0 +1,16 @@
+﻿Set-Location C:\
+Clear-Host
+
+#The SahrePoint Module
+Install-Module -Name Microsoft.Online.SharePoint.PowerShell
+
+#Credential
+$cred = get-credential
+
+#Let's connect
+Connect-SPOService `
+    -Url https://<SP Admin Center>.sharepoint.com `
+    -Credential $cred
+
+#Did it work?
+Get-SPOSite
